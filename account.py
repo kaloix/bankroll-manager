4d9ce55e9fa424fb895cc0e77d006f21dc43423a
Stefan Schindler <stefan@kaloix.de>
2016-05-01 13:43:24 +0200

    reformat code

--- account.py
+++ account.py
@@ -7,7 +7,6 @@ import json
 import logging
 import os.path
 
-
 BB_PER_BUYIN = 100
 PATH = os.path.dirname(os.path.realpath(__file__))
 
@@ -15,154 +14,152 @@ Record = namedtuple('Record', ['timestamp', 'balance'])
 
 
 class Manager(object):
+    def __init__(self):
+        self.filename = os.path.join(PATH, 'state.json')
+        with open(self.filename) as file:
+            state = json.loads(file.read())
+        self.accounts = dict()
+        for name, attr in state['accounts'].items():
+            self.accounts[name] = Account(name=name, **attr)
+        self._selected = state['selected']
 
-	def __init__(self):
-		self.filename = os.path.join(PATH, 'state.json')
-		with open(self.filename) as file:
-			state = json.loads(file.read())
-		self.accounts = dict()
-		for name, attr in state['accounts'].items():
-			self.accounts[name] = Account(name=name, **attr)
-		self._selected = state['selected']
+    @property
+    def listing(self):
+        return sorted(self.accounts)
 
-	@property
-	def listing(self):
-		return sorted(self.accounts)
+    @property
+    def selected(self):
+        return self._selected
 
-	@property
-	def selected(self):
-		return self._selected
+    @selected.setter
+    def selected(self, name):
+        logging.info('select {}'.format(name))
+        self._selected = name
+        self._save()
 
-	@selected.setter
-	def selected(self, name):
-		logging.info('select {}'.format(name))
-		self._selected = name
-		self._save()
+    @property
+    def balance(self):
+        account = self.accounts[self._selected]
+        return account.balance
 
-	@property
-	def balance(self):
-		account = self.accounts[self._selected]
-		return account.balance
+    @balance.setter
+    def balance(self, value):
+        account = self.accounts[self._selected]
+        account.balance = value
+        self._save()
 
-	@balance.setter
-	def balance(self, value):
-		account = self.accounts[self._selected]
-		account.balance = value
-		self._save()
+    @property
+    def stakes(self):
+        account = self.accounts[self._selected]
+        return account.stakes
 
-	@property
-	def stakes(self):
-		account = self.accounts[self._selected]
-		return account.stakes
+    def change(self, period):
+        account = self.accounts[self._selected]
+        if period == 'hour':
+            return account.change(dt.timedelta(hours=1))
+        if period == 'day':
+            return account.change(dt.timedelta(days=1))
+        if period == 'week':
+            return account.change(dt.timedelta(days=7))
+        if period == 'month':
+            return account.change(dt.timedelta(days=30.44))
+        if period == 'year':
+            return account.change(dt.timedelta(days=365.2))
 
-	def change(self, period):
-		account = self.accounts[self._selected]
-		if period == 'hour':
-			return account.change(dt.timedelta(hours=1))
-		if period == 'day':
-			return account.change(dt.timedelta(days=1))
-		if period == 'week':
-			return account.change(dt.timedelta(days=7))
-		if period == 'month':
-			return account.change(dt.timedelta(days=30.44))
-		if period == 'year':
-			return account.change(dt.timedelta(days=365.2))
-
-	def _save(self):
-		logging.debug('save state')
-		accounts = {name: account.state
-		            for name, account in self.accounts.items()}
-		state = {'accounts': accounts,
-		         'selected': self.selected}
-		account_json = json.dumps(state, sort_keys=True, indent='\t')
-		with open(self.filename, 'w') as file:
-			file.write(account_json)
+    def _save(self):
+        logging.debug('save state')
+        accounts = {name: account.state
+                    for name, account in self.accounts.items()}
+        state = {'accounts': accounts,
+                 'selected': self.selected}
+        account_json = json.dumps(state, sort_keys=True, indent='\t')
+        with open(self.filename, 'w') as file:
+            file.write(account_json)
 
 
 class Account(object):
+    def __init__(self, name, currency, precision, buy_ins):
+        self.name = name
+        self.currency = currency
+        self.precision = precision
+        self.buy_ins = buy_ins
+        self.history = list()
+        self.filename = os.path.join(PATH, self.name + '.csv')
+        with suppress(FileNotFoundError), \
+             open(self.filename, newline='') as file:
+            reader = csv.reader(file)
+            for isotime, value in reader:
+                timestamp = dt.datetime.strptime(
+                    isotime[::-1].replace(':', '', 1)[::-1],
+                    '%Y-%m-%dT%H:%M:%S%z')
+                balance = self._cent(decimal.Decimal(value))
+                self.history.append(Record(timestamp, balance))
 
-	def __init__(self, name, currency, precision, buy_ins):
-		self.name = name
-		self.currency = currency
-		self.precision = precision
-		self.buy_ins = buy_ins
-		self.history = list()
-		self.filename = os.path.join(PATH, self.name+'.csv')
-		with suppress(FileNotFoundError), \
-				open(self.filename, newline='') as file:
-			reader = csv.reader(file)
-			for isotime, value in reader:
-				timestamp = dt.datetime.strptime(
-					isotime[::-1].replace(':', '', 1)[::-1],
-					'%Y-%m-%dT%H:%M:%S%z')
-				balance = self._cent(decimal.Decimal(value))
-				self.history.append(Record(timestamp, balance))
+    @property
+    def state(self):
+        return {'currency': self.currency,
+                'precision': self.precision,
+                'buy_ins': self.buy_ins}
 
-	@property
-	def state(self):
-		return {'currency': self.currency,
-		        'precision': self.precision,
-		        'buy_ins': self.buy_ins}
+    @property
+    def balance(self):
+        if not self.history:
+            return '—'
+        return '{}{:,}'.format(self.currency, self.history[-1].balance)
 
-	@property
-	def balance(self):
-		if not self.history:
-			return '—'
-		return '{}{:,}'.format(self.currency, self.history[-1].balance)
+    @balance.setter
+    def balance(self, value):
+        logging.info('{}: set balance {}'.format(self.name, value))
+        new_balance = self._parse(value)
+        self._save(new_balance)
 
-	@balance.setter
-	def balance(self, value):
-		logging.info('{}: set balance {}'.format(self.name, value))
-		new_balance = self._parse(value)
-		self._save(new_balance)
+    @property
+    def stakes(self):
+        if not self.history:
+            return '—'
+        sb = self._cent(self.history[-1].balance / self.buy_ins /
+                        BB_PER_BUYIN / 2)
+        bb = sb * 2
+        buy_in = bb * BB_PER_BUYIN
+        return '{0}{1:,} / {0}{2:,} / {0}{3:,}'.format(
+            self.currency, sb, bb, buy_in)
 
-	@property
-	def stakes(self):
-		if not self.history:
-			return '—'
-		sb = self._cent(self.history[-1].balance / self.buy_ins /
-		                BB_PER_BUYIN / 2)
-		bb = sb * 2
-		buy_in = bb * BB_PER_BUYIN
-		return '{0}{1:,} / {0}{2:,} / {0}{3:,}'.format(
-			self.currency, sb, bb, buy_in)
+    def change(self, timedelta):
+        if not self.history:
+            return '—'
+        start = dt.datetime.now(tz=dt.timezone.utc) - timedelta
+        for timestamp, value in reversed(self.history):
+            before = value
+            if timestamp < start:
+                break
+        delta = self.history[-1].balance - before
+        if before and delta:
+            percent = 100 * float(delta) / float(before)
+            percent = ' ({:.0f}%)'.format(abs(percent))
+        else:
+            percent = str()
+        sign = '' if not delta else '– ' if delta.is_signed() else '+ '
+        return '{}{}{:,}{}'.format(sign, self.currency, abs(delta), percent)
 
-	def change(self, timedelta):
-		if not self.history:
-			return '—'
-		start = dt.datetime.now(tz=dt.timezone.utc) - timedelta
-		for timestamp, value in reversed(self.history):
-			before = value
-			if timestamp < start:
-				break
-		delta = self.history[-1].balance - before
-		if before and delta:
-			percent = 100 * float(delta) / float(before)
-			percent = ' ({:.0f}%)'.format(abs(percent))
-		else:
-			percent = str()
-		sign = '' if not delta else '– ' if delta.is_signed() else '+ '
-		return '{}{}{:,}{}'.format(sign, self.currency, abs(delta), percent)
+    def _save(self, balance):
+        now = dt.datetime.now(tz=dt.timezone.utc)
+        now = now.replace(microsecond=0)
+        self.history.append(Record(now, balance))
+        row = now.isoformat(), str(balance)
+        with open(self.filename, 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(row)
 
-	def _save(self, balance):
-		now = dt.datetime.now(tz=dt.timezone.utc)
-		now = now.replace(microsecond=0)
-		self.history.append(Record(now, balance))
-		row = now.isoformat(), str(balance)
-		with open(self.filename, 'a', newline='') as file:
-			writer = csv.writer(file)
-			writer.writerow(row)
+    def _cent(self, value):
+        exp = decimal.Decimal(str(10 ** -self.precision))
+        return value.quantize(exp, rounding=decimal.ROUND_DOWN)
 
-	def _cent(self, value):
-		exp = decimal.Decimal(str(10 ** -self.precision))
-		return value.quantize(exp, rounding=decimal.ROUND_DOWN)
-
-	def _parse(self, value):
-		try:
-			value = decimal.Decimal(value)
-		except decimal.InvalidOperation as err:
-			raise ValueError('parsing error') from err
-		rounded = self._cent(value)
-		if rounded != value:
-			raise ValueError('precision too high')
-		return rounded
+    def _parse(self, value):
+        try:
+            value = decimal.Decimal(value)
+        except decimal.InvalidOperation as err:
+            raise ValueError('parsing error') from err
+        rounded = self._cent(value)
+        if rounded != value:
+            raise ValueError('precision too high')
+        return rounded
--- main.py
+++ main.py
@@ -5,11 +5,10 @@ import os.path
 import sys
 
 from PyQt5.QtWidgets import QApplication, QComboBox, QGridLayout, QLabel, \
-	QLineEdit, QVBoxLayout, QWidget
+    QLineEdit, QVBoxLayout, QWidget
 from PyQt5.QtGui import QIcon
 
 import account
-
 
 PATH = os.path.dirname(os.path.realpath(__file__))
 STYLE = {'green': 'color : DarkGreen;',
@@ -18,108 +17,106 @@ STYLE = {'green': 'color : DarkGreen;',
 
 
 def main():
-	logging.basicConfig(
-		format='[%(asctime)s|%(levelname)s|%(module)s] %(message)s',
-		datefmt='%H:%M:%S', level=logging.INFO)
-	app = QApplication(sys.argv)
-	easy_test = Application()
-	sys.exit(app.exec_())
+    logging.basicConfig(
+        format='[%(asctime)s|%(levelname)s|%(module)s] %(message)s',
+        datefmt='%H:%M:%S', level=logging.INFO)
+    app = QApplication(sys.argv)
+    easy_test = Application()
+    sys.exit(app.exec_())
 
 
 class BankrollManager(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Poker Bankroll Manager')
+        self.setWindowIcon(QIcon(os.path.join(PATH, 'icon.png')))
+        self.widget = dict()
+        self.create_widgets()
+        self.show()
 
-	def __init__(self):
-		super().__init__()
-		self.setWindowTitle('Poker Bankroll Manager')
-		self.setWindowIcon(QIcon(os.path.join(PATH, 'icon.png')))
-		self.widget = dict()
-		self.create_widgets()
-		self.show()
+    def create_widgets(self):
+        self.layout = QGridLayout()
+        self.setLayout(self.layout)
+        self.row_elems = int()
+        self.add_combo('Account')
+        self.add_edit('New Balance')
+        self.add_label('Balance')
+        self.add_label('Stakes')
+        self.add_label('Last Hour')
+        self.add_label('Last Day')
+        self.add_label('Last Week')
+        self.add_label('Last Month')
+        self.add_label('Last Year')
 
-	def create_widgets(self):
-		self.layout = QGridLayout()
-		self.setLayout(self.layout)
-		self.row_elems = int()
-		self.add_combo('Account')
-		self.add_edit('New Balance')
-		self.add_label('Balance')
-		self.add_label('Stakes')
-		self.add_label('Last Hour')
-		self.add_label('Last Day')
-		self.add_label('Last Week')
-		self.add_label('Last Month')
-		self.add_label('Last Year')
+    def add_label(self, text):
+        self.widget[text] = elem = QLabel(self)
+        self.add_element(text, elem)
 
-	def add_label(self, text):
-		self.widget[text] = elem = QLabel(self)
-		self.add_element(text, elem)
+    def add_combo(self, text):
+        self.widget[text] = elem = QComboBox(self)
+        self.add_element(text, elem)
 
-	def add_combo(self, text):
-		self.widget[text] = elem = QComboBox(self)
-		self.add_element(text, elem)
+    def add_edit(self, text):
+        self.widget[text] = elem = QLineEdit(self)
+        self.add_element(text, elem)
 
-	def add_edit(self, text):
-		self.widget[text] = elem = QLineEdit(self)
-		self.add_element(text, elem)
-
-	def add_element(self, text, elem):
-		name = QLabel(text+':', self)
-		self.layout.addWidget(name, self.row_elems, 0)
-		self.layout.addWidget(elem, self.row_elems, 1)
-		self.row_elems += 1
+    def add_element(self, text, elem):
+        name = QLabel(text + ':', self)
+        self.layout.addWidget(name, self.row_elems, 0)
+        self.layout.addWidget(elem, self.row_elems, 1)
+        self.row_elems += 1
 
 
 class Application(BankrollManager):
+    def __init__(self):
+        super().__init__()
+        self.manager = account.Manager()
+        self.fill_combos()
+        self.load()
+        self.connect_methods()
 
-	def __init__(self):
-		super().__init__()
-		self.manager = account.Manager()
-		self.fill_combos()
-		self.load()
-		self.connect_methods()
+    def fill_combos(self):
+        self.widget['Account'].clear()
+        self.widget['Account'].addItems(self.manager.listing)
 
-	def fill_combos(self):
-		self.widget['Account'].clear()
-		self.widget['Account'].addItems(self.manager.listing)
+    def connect_methods(self):
+        self.widget['Account'].currentTextChanged.connect(self.select)
+        self.widget['New Balance'].returnPressed.connect(self.set_balance)
 
-	def connect_methods(self):
-		self.widget['Account'].currentTextChanged.connect(self.select)
-		self.widget['New Balance'].returnPressed.connect(self.set_balance)
+    def select(self, account):
+        self.manager.selected = account
+        self.load()
 
-	def select(self, account):
-		self.manager.selected = account
-		self.load()
+    def load(self):
+        account = self.manager.selected
+        logging.debug('load {}'.format(account))
+        self.widget['Account'].setCurrentText(account)
+        self.widget['Balance'].setText(self.manager.balance)
+        self.widget['Stakes'].setText(self.manager.stakes)
+        for period in ['hour', 'day', 'week', 'month', 'year']:
+            key = 'Last ' + period.capitalize()
+            change = self.manager.change(period)
+            if change[0] == '+':
+                color = 'green'
+            elif change[0] == '–':
+                color = 'red'
+            else:
+                color = 'default'
+            self.widget[key].setText(change)
+            self.widget[key].setStyleSheet(STYLE[color])
+        self.widget['New Balance'].setFocus()
+        self.setFixedSize(self.sizeHint())
 
-	def load(self):
-		account = self.manager.selected
-		logging.debug('load {}'.format(account))
-		self.widget['Account'].setCurrentText(account)
-		self.widget['Balance'].setText(self.manager.balance)
-		self.widget['Stakes'].setText(self.manager.stakes)
-		for period in ['hour', 'day', 'week', 'month', 'year']:
-			key = 'Last ' + period.capitalize()
-			change = self.manager.change(period)
-			if change[0] == '+':
-				color = 'green'
-			elif change[0] == '–':
-				color = 'red'
-			else:
-				color = 'default'
-			self.widget[key].setText(change)
-			self.widget[key].setStyleSheet(STYLE[color])
-		self.widget['New Balance'].setFocus()
-		self.setFixedSize(self.sizeHint())
-
-	def set_balance(self):
-		value = self.widget['New Balance'].text()
-		try:
-			self.manager.balance = value
-		except ValueError as err:
-			logging.warning('{}: {}'.format(type(err).__name__, err))
-			return
-		self.widget['New Balance'].clear()
-		self.load()
+    def set_balance(self):
+        value = self.widget['New Balance'].text()
+        try:
+            self.manager.balance = value
+        except ValueError as err:
+            logging.warning('{}: {}'.format(type(err).__name__, err))
+            return
+        self.widget['New Balance'].clear()
+        self.load()
 
 
 if __name__ == '__main__':
-	main()
+    main()
